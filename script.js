5658722608bbf851ccaf37fe957cedac1e1aa589
Mansi Kalra <54201347+mansikalraa@users.noreply.github.com>
2022-01-15 18:39:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ let checkAbo = document.getElementById("check-about");
 let checkSkill = document.getElementById("check-skill");
 let checkCon = document.getElementById("check-contact");
 
-console.log(checkPro.value);
-console.log(checkAbo.value);
-console.log(checkSkill.value);
-console.log(checkCon.value);
+
 
 checkPro.addEventListener("click", function() {
     if (checkPro.checked) {
@@ -37,3 +34,75 @@ checkCon.addEventListener("click", function() {
         checkAbo.checked = false;
     }
 });
+
+
+// form validations
+
+let yname = document.getElementById("name");
+let email = document.getElementById("email");
+let msg = document.getElementById("msg");
+let sendbtn = document.getElementById("sendbtn");
+let sendmsg = document.getElementById("sendmsg");
+
+sendbtn.addEventListener("click", function() {
+    let i = validations();
+    if (i == 1) {
+        sendEmail();
+    }
+});
+
+function validations() {
+    yname.style.borderColor = "initial";
+    email.style.borderColor = "initial";
+    msg.style.borderColor = "initial";
+    if (yname.value == '') {
+        yname.placeholder = "Name?";
+        yname.style.borderColor = "red";
+        return 0;
+    }
+    if (email.value == '') {
+        email.placeholder = "Email?";
+        email.style.borderColor = "red";
+        return 0;
+    }
+    if (email.value != '') {
+        let atpos = email.value.indexOf("@");
+        let dotpos = email.value.lastIndexOf(".");
+
+        if (atpos < 1 || (dotpos - atpos < 2)) {
+            email.value = '';
+            email.placeholder = "Incorrect Email.";
+            email.focus();
+            email.style.borderColor = "red";
+            return 0;
+        }
+    }
+
+    if (msg.value == '') {
+        msg.placeholder = "Message?";
+        msg.style.borderColor = "red";
+        return 0;
+    }
+    return 1;
+}
+
+function sendEmail() {
+    let sub = "Message from " + yname.value;
+    let bod = "Name : " + yname.value + "\nEmail : " + email.value + "\nMessage : " + msg.value;
+
+    Email.send({
+            Host: "smtp.gmail.com",
+            Username: "mansiwebsitequiries@gmail.com",
+            Password: "idontknowwhattodo",
+            To: 'mansi.kalra23@gmail.com',
+            From: "mansiwebsitequiries@gmail.com",
+            Subject: sub,
+            Body: bod,
+        })
+        .then(function(message) {
+            yname.value = '';
+            email.value = '';
+            msg.value = '';
+            sendmsg.innerHTML = "Message sent!";
+        });
+}
